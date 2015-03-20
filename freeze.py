2fdd61b541e9403a0f0a0173ad871e5322b3612f
sstirling <stephenstirling@gmail.com>
2015-03-20 18:44:55 -0400

    Froze my app

--- freeze.py
+++ freeze.py
@@ -3,12 +3,11 @@ from app import app, get_csv
 from app import app
 freezer = Freezer(app)
 
-
 @freezer.register_generator
 def detail():
     for row in get_csv():
         yield {'row_id': row['id']}
-        
+
 
 if __name__ == '__main__':
     freezer.freeze()
